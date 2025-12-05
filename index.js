1d91ba7ab1a89a5d7a3bf8da61bb2d70cb71c044
mdriyazakondo <mdriyazakondo265@gmail.com>
2025-12-06 05:21:02 +0600

    book status update api

--- index.js
+++ index.js
@@ -97,6 +97,20 @@ async function run() {
       res.send(result);
     });
 
+    //==== publish and unpublish =========//
+    app.patch("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          create_date: new Date(),
+          status: req.body.status,
+        },
+      };
+      const result = await bookCollection.updateOne(query, updateDoc);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
